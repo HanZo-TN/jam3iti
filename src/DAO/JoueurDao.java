a9794ea9c461cc313bcf3abd615ea864d4ee100c
HanZo-TN <mohamedamine.rammeh@esprit.tn>
2013-05-18 17:18:52 +0100

    ajout de la methode create dans la classe JoueurDao

--- src/DAO/JoueurDao.java
+++ src/DAO/JoueurDao.java
@@ -5,6 +5,9 @@
 package DAO;
 
 import Metier.Joueur;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,7 +25,15 @@ public class JoueurDao extends DaoAbstraite<Joueur>{
 
     @Override
     public void create() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Statement st;
+        try {
+            st = this.connect().createStatement();
+            st.executeUpdate("create table Joueur(id int(4) not null, nom varchar(30) not null, prenom varchar(30) not null,email varchar(30) not null, post varchar(30) not null, salaire float not null, taille float not null, poid float not null, datenaiss date not null );");
+            System.out.println("la table Joueur est creer");
+        } catch (SQLException ex) {
+            Logger.getLogger(JoueurDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+			
     }
 
     @Override

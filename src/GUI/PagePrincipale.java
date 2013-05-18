b1b539241dd7a95170267982cc81c52a763ae380
Alexandre BM <s@rednaks.tn>
2013-05-18 13:50:07 +0100

    Ajout Menu gauche + Sondage

--- src/GUI/PagePrincipale.java
+++ src/GUI/PagePrincipale.java
@@ -27,6 +27,19 @@ public class PagePrincipale extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroupSondageChoix = new javax.swing.ButtonGroup();
+        jButtonNextMatch = new javax.swing.JButton();
+        jButtonLastMatch = new javax.swing.JButton();
+        jButtonClassement = new javax.swing.JButton();
+        jButtonCalendrier = new javax.swing.JButton();
+        jPanelSondage = new javax.swing.JPanel();
+        jLabelSondage = new javax.swing.JLabel();
+        jRadioButtonSondageChoix1 = new javax.swing.JRadioButton();
+        jRadioButtonSondageChoix2 = new javax.swing.JRadioButton();
+        jRadioButtonSondageChoix3 = new javax.swing.JRadioButton();
+        jButtonVote = new javax.swing.JButton();
+        jButtonRes = new javax.swing.JButton();
+        jLabelSondageQuestion = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenuAcceuil = new javax.swing.JMenu();
         jMenuFoot = new javax.swing.JMenu();
@@ -37,6 +50,71 @@ public class PagePrincipale extends javax.swing.JFrame {
         jMenuContact = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButtonNextMatch.setText("Prochain Match");
+
+        jButtonLastMatch.setText("Dernier Match");
+
+        jButtonClassement.setText("Classement");
+
+        jButtonCalendrier.setText("Calendrier");
+
+        jLabelSondage.setText("Sondage");
+
+        buttonGroupSondageChoix.add(jRadioButtonSondageChoix1);
+        jRadioButtonSondageChoix1.setText("Choix 1");
+
+        buttonGroupSondageChoix.add(jRadioButtonSondageChoix2);
+        jRadioButtonSondageChoix2.setText("Choix 2");
+
+        jRadioButtonSondageChoix3.setText("Choix 3");
+
+        jButtonVote.setText("Vote");
+
+        jButtonRes.setText("Resultat");
+
+        jLabelSondageQuestion.setText("Question ?");
+
+        javax.swing.GroupLayout jPanelSondageLayout = new javax.swing.GroupLayout(jPanelSondage);
+        jPanelSondage.setLayout(jPanelSondageLayout);
+        jPanelSondageLayout.setHorizontalGroup(
+            jPanelSondageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelSondageLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanelSondageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanelSondageLayout.createSequentialGroup()
+                        .addComponent(jButtonVote)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonRes))
+                    .addComponent(jRadioButtonSondageChoix2)
+                    .addComponent(jRadioButtonSondageChoix3)
+                    .addGroup(jPanelSondageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jLabelSondageQuestion)
+                        .addComponent(jRadioButtonSondageChoix1)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelSondageLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabelSondage)
+                .addGap(33, 33, 33))
+        );
+        jPanelSondageLayout.setVerticalGroup(
+            jPanelSondageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelSondageLayout.createSequentialGroup()
+                .addComponent(jLabelSondage)
+                .addGap(7, 7, 7)
+                .addComponent(jLabelSondageQuestion)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jRadioButtonSondageChoix1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jRadioButtonSondageChoix2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jRadioButtonSondageChoix3)
+                .addGap(18, 18, 18)
+                .addGroup(jPanelSondageLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonVote)
+                    .addComponent(jButtonRes))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         jMenuAcceuil.setText("Acceuil");
         jMenuAcceuil.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -71,11 +149,33 @@ public class PagePrincipale extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 531, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButtonNextMatch, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButtonLastMatch, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButtonClassement, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButtonCalendrier, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 291, Short.MAX_VALUE)
+                .addComponent(jPanelSondage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 289, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(65, 65, 65)
+                        .addComponent(jButtonNextMatch)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonLastMatch)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButtonClassement)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButtonCalendrier))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addComponent(jPanelSondage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(63, Short.MAX_VALUE))
         );
 
         pack();
@@ -121,6 +221,15 @@ public class PagePrincipale extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroupSondageChoix;
+    private javax.swing.JButton jButtonCalendrier;
+    private javax.swing.JButton jButtonClassement;
+    private javax.swing.JButton jButtonLastMatch;
+    private javax.swing.JButton jButtonNextMatch;
+    private javax.swing.JButton jButtonRes;
+    private javax.swing.JButton jButtonVote;
+    private javax.swing.JLabel jLabelSondage;
+    private javax.swing.JLabel jLabelSondageQuestion;
     private javax.swing.JMenu jMenuAcceuil;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenu jMenuBaske;
@@ -129,5 +238,9 @@ public class PagePrincipale extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenu jMenuPresse;
     private javax.swing.JMenu jMenuStaff;
+    private javax.swing.JPanel jPanelSondage;
+    private javax.swing.JRadioButton jRadioButtonSondageChoix1;
+    private javax.swing.JRadioButton jRadioButtonSondageChoix2;
+    private javax.swing.JRadioButton jRadioButtonSondageChoix3;
     // End of variables declaration//GEN-END:variables
 }

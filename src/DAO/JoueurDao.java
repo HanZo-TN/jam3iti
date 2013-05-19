4cfa89a4586f634ec8a0eff836abf1ca7d597861
HanZo-TN <mohamedamine.rammeh@esprit.tn>
2013-05-19 09:26:37 +0100

    ajout de la methode find all dans JoueurDao

--- src/DAO/JoueurDao.java
+++ src/DAO/JoueurDao.java
@@ -6,7 +6,7 @@ package DAO;
 
 import Metier.Joueur;
 import java.sql.*;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -172,7 +172,42 @@ public class JoueurDao extends DaoAbstraite<Joueur>{
 
     @Override
     public List<Joueur> findAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Joueur> listJ = new ArrayList<Joueur>();
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            st = this.connect().createStatement();
+            rs = st.executeQuery("select * from Joueur");
+            System.out.println("recherche général effectuée");
+            
+            
+            while (rs.next()) {
+              
+                
+                Joueur jr = new Joueur(rs.getInt("id"), rs.getString("nom"),rs.getString("prenom"),rs.getDate("datenaiss"),rs.getString("post"),rs.getFloat("salaire"),rs.getFloat("taille"),rs.getFloat("poid"),rs.getString("email"));
+                
+                
+               
+                               listJ.add(jr);
+            }
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(JoueurDao.class.getName()).log(Level.SEVERE, "recherche general echoué", ex);
+        }finally{
+            try {
+                if(rs != null)
+                rs.close();
+                if(st != null)
+                st.close();
+                
+            } catch (SQLException ex) {
+                Logger.getLogger(JoueurDao.class.getName()).log(Level.SEVERE, "liberation statement || resultset echoué", ex);
+            }
+        }
+        
+        
+        return listJ;
     }
     
 }

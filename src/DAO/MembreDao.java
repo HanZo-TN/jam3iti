750c28d1047fdc9778efa3a6650e0e3415222c02
HanZo-TN <mohamedamine.rammeh@esprit.tn>
2013-05-19 21:46:59 +0100

    ajout de la methode insert  dans la classe MembreDao

--- src/DAO/MembreDao.java
+++ src/DAO/MembreDao.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package DAO;
-import Metier.Staff;
+import Metier.Membre;
 import java.util.*;
 import java.sql.*;
 import java.util.logging.Level;
@@ -13,7 +13,7 @@ import java.util.logging.Logger;
  *
  * @author rmh
  */
-public class MembreDao extends DaoAbstraite<Staff>{
+public class MembreDao extends DaoAbstraite<Membre>{
     
          private MembreDao () {}
     public static MembreDao instance;
@@ -36,27 +36,54 @@ public class MembreDao extends DaoAbstraite<Staff>{
     }
 
     @Override
-    public Staff insert(Staff obj) {
+    public Membre insert(Membre obj) {
+        PreparedStatement pst = null;
+        try {
+                            pst = this.connect().prepareStatement("INSERT INTO Membre (email,nom,prenom,mdp,status,id) VALUES (?,?,?,?,?,?);");
+                            pst.setString(1, obj.getEmail());
+                            pst.setString(2, obj.getNomMembre());
+                            pst.setString(3, obj.getPrenomMembre());
+                            pst.setString(4, obj.getMdp());
+                            pst.setString(5, obj.getStatus());
+                            pst.setInt(6, obj.getIdMembre());
+                            
+                            pst.executeUpdate();
+                            System.out.println("insertion Membre terminer");
+        } catch (SQLException ex) {
+            Logger.getLogger(MembreDao.class.getName()).log(Level.SEVERE, "insertion echouer", ex);
+        }finally{
+        
+            try {
+            if(pst != null)
+            pst.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(MembreDao.class.getName()).log(Level.SEVERE, "liberation du preparedstatement echouée", ex);
+        }
+            
+        }
+        
+        
+        
+        return obj;
+    }
+
+    @Override
+    public void delete(Membre obj) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void delete(Staff obj) {
+    public void update(Membre obj) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void update(Staff obj) {
+    public Membre find(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public Staff find(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<Staff> findAll() {
+    public List<Membre> findAll() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
